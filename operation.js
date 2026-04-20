ec942750ee92a262ac10d4911bbc85fa01475b2f
Diptesh8 <shiva14420065@gmail.com>
2026-04-20 09:29:46 +0530

    added new price and discount code

---
+++ operation.js
@@ -0,0 +1,14 @@
+var eggprice =20;
+var oninionprice =10;
+console.log(eggprice + oninionprice);
+console.log(oninionprice * eggprice);
+console.log(eggprice - oninionprice);
+console.log(eggprice / oninionprice);
+console.log(10 % 3);
+
+var price =40;
+// price =price + 10;
+// console.log(price);
+var newprice =price + 50;
+var discountprice =newprice - 20;
+console.log(discountprice);
