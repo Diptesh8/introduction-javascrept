6cfbcf169c6e588fb4806b5d7c6b7bbf3d03e267
Diptesh8 <shiva14420065@gmail.com>
2026-04-20 08:46:28 +0530

    added some new code

--- number.js
+++ number.js
@@ -1,4 +1,6 @@
 var orrangeprice = 20;
-var appleprice = 20;
+var appleprice = '20';
 var cow = 3.7;
+console.log(typeof appleprice);
+console.log(typeof orrangeprice);
 console.log(orrangeprice + appleprice);
