9d4d99e9eba2531a2d60578618dc3b29a68caf78
Diptesh8 <shiva14420065@gmail.com>
2026-04-20 09:06:58 +0530

    added operation.js file

--- naming.js
+++
@@ -1,6 +0,0 @@
-
-var apple = 34;
-var chocelet = 20;
-var money = 500;
-console.log(chocelet + apple);
-console.log(typeof money);
--- number.js
+++ number.js
@@ -1,6 +1,13 @@
 var orrangeprice = 20;
-var appleprice = '20';
+var appleprice = parseInt( '20' ) ;
 var cow = 3.7;
 console.log(typeof appleprice);
 console.log(typeof orrangeprice);
 console.log(orrangeprice + appleprice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second ;
+console.log(total);
+const num = -50 /0 ;
+console.log(num);
