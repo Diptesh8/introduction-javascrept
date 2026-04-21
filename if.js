6a5648fdf45269942a6f23cc0d46538ad1c16360
Diptesh8 <shiva14420065@gmail.com>
2026-04-21 08:40:14 +0530

    added some new code

---
+++ if.js
@@ -0,0 +1,11 @@
+briyaniprice =200;
+if(briyaniprice < 250){
+    console.log("so mama give me briyani  packet");
+}
+var weight = 19;
+if(weight <20){
+    console.log('than  i carry this rice bag');
+}
+else{
+    console.log('otherwise i  call a riskha and he carry the rice bag')
+}
