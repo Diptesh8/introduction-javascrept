75d294df0e7f48d06741b417335e00b62f9e4cb1
Diptesh8 <shiva14420065@gmail.com>
2026-04-21 19:59:20 +0530

    added else if syntax code

--- if.js
+++ if.js
@@ -10,11 +10,24 @@
 //     console.log('otherwise i  call a riskha and he carry the rice bag')
 // }
 
-const salary = 25000;
-const height = 67;
-if(salary > 20000 && height > 66){
-    console.log('he is a supatra');
+const salary = 8000;
+const height = 63;
+// if(salary > 20000 && height > 66){
+//     console.log('he is a supatra');
+// }
+// else{
+//     console.log('find the next patra ');
+// }
+if(salary > 20000 || height > 62){
+    console.log('chalano jai ei patra ')
 }
 else{
-    console.log('find the next patra ');
+    console.log('next patre er dike jetye hobe');
+}
+// multilabe concept
+const price = 35000 ;
+if (price >= 5000){
+    const discunt = price * 10 / 100 ;
+    const payammount = price - discunt ;
+    console.log(payammount);
 }
