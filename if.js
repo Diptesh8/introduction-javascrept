535dc1a6800854f7c22952305d150ff73fdfe78a
Diptesh8 <shiva14420065@gmail.com>
2026-04-21 08:55:16 +0530

    add if else condition

--- if.js
+++ if.js
@@ -1,11 +1,20 @@
-briyaniprice =200;
-if(briyaniprice < 250){
-    console.log("so mama give me briyani  packet");
-}
-var weight = 19;
-if(weight <20){
-    console.log('than  i carry this rice bag');
+// briyaniprice =200;
+// if(briyaniprice < 250){
+//     console.log("so mama give me briyani  packet");
+// }
+// var weight = ;
+// if(weight <20){
+//     console.log('than  i carry this rice bag');
+// }
+// else{
+//     console.log('otherwise i  call a riskha and he carry the rice bag')
+// }
+
+const salary = 25000;
+const height = 67;
+if(salary > 20000 && height > 66){
+    console.log('he is a supatra');
 }
 else{
-    console.log('otherwise i  call a riskha and he carry the rice bag')
+    console.log('find the next patra ');
 }
