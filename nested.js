e4192235cbc3b0b55e2f903f036ab3150028a2fb
Diptesh8 <shiva14420065@gmail.com>
2026-04-22 07:33:03 +0530

    added nested  loop

---
+++ nested.js
@@ -0,0 +1,26 @@
+const  price = 10;
+if(price >= 500) {
+    console.log('bro you are boro lok');
+}
+else{
+    if(price >= 300){
+        console.log('bro you are borolok na gorib na');
+    }
+    else{
+  if(price >= 100){
+    console.log('tumi gogib hoye gacho');
+  }
+  
+  else 
+    if(price >= 50) {
+ console.log('tui amr bondhu na');
+  }
+  else
+    if(price >= 20) {
+        console.log('ami toder taks debo 20 taka');
+    }
+    else
+        console.log('jodi jodi sei tare kotha bolo');
+   
+}
+}
