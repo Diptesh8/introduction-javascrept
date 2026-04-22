279686d03191693cf0615e822b28987b98b85b05
Diptesh8 <shiva14420065@gmail.com>
2026-04-22 19:56:29 +0530

    added new file

---
+++ ternary.js
@@ -0,0 +1,12 @@
+// TERNARY OPERATOR ( ? : )
+
+// const age = 107;
+// (age >= 18) ?console.log ('you can go vote') :console.log('tumi ghumim e poro') ;
+
+const isPassed = false;
+if (!isPassed){
+    console.log('tumi pas kore valo number paiycho');
+}
+else{
+    console.log('tumi r pass korte parbe na ');
+}
